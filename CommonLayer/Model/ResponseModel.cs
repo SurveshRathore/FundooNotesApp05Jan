9eea109d5fa8befedd59bf94ca0292222562ec5a
Survesh Rathore <113424413+SurveshRathore@users.noreply.github.com>
2023-09-13 17:07:17 +0530

    [Survesh] Added: Response Model Class

--- CommonLayer/Model/ResponseModel.cs
+++ CommonLayer/Model/ResponseModel.cs
@@ -9,6 +9,7 @@ namespace CommonLayer.Model
     public class ResponseModel <T>
     {
         public bool Status {get; set;}
+        
         public String Message { get; set;}
 
         public T Data { get; set;}
